d698f37f9a1a1f4b093d9d52b8d0611fedb5a386
iumakaro <ninoDelLunes@yandex.ru>
2021-10-22 00:14:46 +0300

    Add ship service

---
+++ src/main/java/ru/levelup/battleship/services/GameService.java
@@ -0,0 +1,17 @@
+package ru.levelup.battleship.services;
+
+import ru.levelup.battleship.model.Game;
+import ru.levelup.battleship.model.User;
+
+public interface GameService {
+
+    Game createGame(User player1, User player2);
+
+    Game endGame(Game game, boolean isCompleted, User winner);
+
+    Game updateGame(Game game, User playerToMove);
+
+    long countGamesByWinner(User user);
+
+    long countAllGamesByUser(User user);
+}
---
+++ src/main/java/ru/levelup/battleship/services/impl/GameServiceImpl.java
@@ -0,0 +1,44 @@
+package ru.levelup.battleship.services.impl;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.levelup.battleship.dao.GamesRepository;
+import ru.levelup.battleship.model.Game;
+import ru.levelup.battleship.model.User;
+import ru.levelup.battleship.services.GameService;
+
+@Service
+@AllArgsConstructor
+public class GameServiceImpl implements GameService {
+
+    private GamesRepository repository;
+
+    @Override
+    @Transactional
+    public Game createGame(User player1, User player2) {
+        return repository.createGame(player1, player2);
+    }
+
+    @Override
+    @Transactional
+    public Game endGame(Game game, boolean isCompleted, User winner) {
+        return repository.endGame(game, true, winner);
+    }
+
+    @Override
+    @Transactional
+    public Game updateGame(Game game, User playerToMove) {
+        return repository.updateGame(game, playerToMove);
+    }
+
+    @Override
+    public long countGamesByWinner(User user) {
+        return repository.countGamesByWinner(user);
+    }
+
+    @Override
+    public long countAllGamesByUser(User user) {
+        return repository.countGamesByUser(user);
+    }
+}
