b471b3bf37bb5c7bdd0e4792c0f3d9b38dc9c9b2
iumakaro <ninoDelLunes@yandex.ru>
2021-11-03 17:32:51 +0300

    Add gameboard.js

---
+++ src/main/resources/static/gameboard.js
@@ -0,0 +1,31 @@
+"use strict";
+const MESSAGE = document.querySelector("#message");
+const OPPONENT_FIELD = document.querySelector("#opponent_board");
+const MY_FIELD = document.querySelector("#my_board");
+
+function arrangeShips() {
+    fetch("/api/arrange/" + LOGIN)
+        .then(response => response.json())
+        .then(entries => {
+            MY_FIELD.querySelectorAll(".ship")
+                .forEach(cell => cell.classList.remove('ship'));
+            entries.forEach(entry => {
+                const cell = MY_FIELD.querySelector(
+                    `td[data-x="${entry.coordinateX}"][data-y="${entry.coordinateY}"]`);
+                cell.classList.add('ship')
+            });
+        }).catch(e => console.log(e));
+}
+
+function initField() {
+    OPPONENT_FIELD.querySelectorAll("td")
+        .forEach(cell => cell.addEventListener("click", event => {
+                const target = {
+                    x: event.target.dataset.x,
+                    y: event.target.dataset.y
+                };
+                console.log(target);
+                event.target.style.backgroundColor = "red";
+            }
+        ));
+}
