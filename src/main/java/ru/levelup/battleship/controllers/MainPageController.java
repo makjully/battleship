0d5b423a13cbef2c1fc935e4c77b9d9ea43d59cc
iumakaro <ninoDelLunes@yandex.ru>
2021-11-03 17:32:23 +0300

    Change MainPageController

--- src/main/java/ru/levelup/battleship/controllers/MainPageController.java
+++ src/main/java/ru/levelup/battleship/controllers/MainPageController.java
@@ -4,28 +4,66 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.*;
+import ru.levelup.battleship.model.Game;
 import ru.levelup.battleship.model.GameRoom;
 import ru.levelup.battleship.model.User;
+import ru.levelup.battleship.services.GameRoomService;
 import ru.levelup.battleship.services.GameService;
 import ru.levelup.battleship.services.UserService;
 
 @Controller
 @AllArgsConstructor
+@RequestMapping("app/main")
 public class MainPageController {
 
     private UserService userService;
     private GameService gameService;
+    private GameRoomService roomService;
+    private static final String MAIN_PAGE = "main";
 
-    @GetMapping("/app/main")
-    public String getMainPage(Model model,
-                              @ModelAttribute("room") GameRoom gameRoom,
-                              Authentication authentication) {
-        User me = userService.findByLogin(authentication.getName());
-        model.addAttribute("me", me);
+    @GetMapping("/{room_id}")
+    public String showMain(Model model,
+                           @PathVariable("room_id") Long roomId,
+                           Authentication authentication) {
 
-        return "main";
+        return MAIN_PAGE;
     }
 
+    @PostMapping("/join")
+    public String joinRoom(@RequestParam("id") Long roomId,
+                           Authentication authentication) {
+        User user = userService.findByLogin(authentication.getName());
+        GameRoom room = roomService.findById(roomId);
+
+        if (room != null && room.getAccepting() == null) {
+            roomService.updateRoomWhenAccept(room, user);
+        } else
+            return "redirect:rooms";
+
+        return "redirect:/" + roomId;
+    }
+
+    @PostMapping("/create")
+    public String createRoom(Model model,
+                             Authentication authentication) {
+        User user = userService.findByLogin(authentication.getName());
+        GameRoom room = roomService.createRoom(user);
+        model.addAttribute("user", user);
+        model.addAttribute("room", room);
+
+        return "redirect:/" + room.getId();
+    }
+
+    @PostMapping("/startGame")
+    public String startGame(Model model,
+                            @ModelAttribute("room") GameRoom room) {
+        User user_1 = room.getInviter();
+        User user_2 = room.getInviter();
+
+        Game game = gameService.createGame(user_1, user_2);
+        model.addAttribute("game", game);
+
+        return MAIN_PAGE;
+    }
 }
